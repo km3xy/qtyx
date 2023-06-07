4956dcc8dcbcc16c870ed45df64368b751e3bb46
km3xy <77920834+km3xy@users.noreply.github.com>
2023-06-07 08:03:43 +0800

    Update 随1全局无限制.php

--- 随1全局无限制.php
+++ 随1全局无限制.php
@@ -1,3 +1,61 @@
+
+
+<h1>
+
+
+第一页,无限制版,随一,定胆随机
+
+
+</h1>
+
+
+​
+<div ng-app="myApp" ng-controller="myCtrl">
+    
+    
+  <p><h3>a_ng后区指定位数 : 
+  
+
+ <input   style="font-size:30px;color:#FF0000" type="text" name="a_ng[]" id="a1" value="2" ng-model="firstName">   
+
+
+        <label for="fullname"><h1>2023年5月1，可指定位数传值：</label>
+       
+</p>
+    
+  <p><h3>_ng前区指定位数 : 
+    
+    
+<input style="font-size:30px;color:#FF0000" type="text" name="b_ng[]" id
+
+="b1" value="6" ng-model="lastName">
+
+<br>
+
+<br>
+
+<h1>
+    
+input前区位数值: {{+lastName}}
+
+<br>
+
+
+<br>
+
+
+input后区位数值: {{+firstName}}
+
+
+
+<br>
+
+<h1>
+    
+<br>
+
+
+
 <?php
 
 
