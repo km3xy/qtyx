40d98490e3d798bd6d5e0ed2d020b504c3d526c9
km3xy <77920834+km3xy@users.noreply.github.com>
2023-06-28 16:48:31 +0800

    Create 随一定胆，随一，随机加入，红黑版.php

---
+++ 随一定胆，随一，随机加入，红黑版.php
@@ -0,0 +1,1593 @@
+
+<html lang="zh-CN">
+    
+<head>
+    
+    
+<meta charset="utf-8">
+
+
+<title>
+
+input免输入版ms5新增，请选择上期码  
+
+随一，定胆
+</title>
+
+    
+
+
+<style>
+
+
+
+p {display: inline;}
+
+
+</style>
+
+
+
+<script src="jquery.min.js">
+</script>
+
+
+
+
+<script>
+
+$(document).ready(function(){
+    
+    
+$("#button3").click(function(){
+ 
+
+var colors = ['a','b','c','d','e','f','g','test1','test2'];
+
+
+
+var x = colors[(Math.floor(Math.random() * (colors.length)))]
+
+
+
+var colorsxy = [,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+
+var xy = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="换";
+
+//$("#to").attr('id','xx');//之后变成xx
+ 
+ 
+ 
+ var $p= Math.floor(Math.random()*35+0);
+
+var $z= Math.floor(Math.random()*35+0);
+
+
+ 
+if ($p<$z)
+{
+    
+$("#"+x).val("换");
+
+}
+else
+{
+
+
+
+$("#"+x).val(y2);
+
+
+  }
+
+
+ 
+ 
+
+ //前区随机替换还原
+
+
+
+
+$("#a").attr('id','f');
+
+$("#b").attr('id','e');
+
+
+$("#c").attr('id','d');
+
+
+$("#d").attr('id','c');
+
+$("#e").attr('id','b');
+
+
+$("#f").attr('id','a');
+
+
+
+
+  });
+    
+    
+    
+    
+    
+    //后区选号//前区附加选号
+    
+    
+  $("#btn1").click(function(){
+    $("#test1").val(Math.floor(Math.random()*35+1));
+  });
+  $("#btn2").click(function(){
+    $("#test2").val(Math.floor(Math.random()*35+1));
+  });
+  
+  
+  
+  
+  //前区选号
+  
+  
+  $("#btn3").click(function(){
+      
+      
+    //前区全局替换  
+      
+var colors = ['a','b','c','d','e','f','g','test1','test2'];
+
+
+
+
+var x = colors[(Math.floor(Math.random() * (colors.length)))]
+
+
+
+
+var colorsxy = [,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+
+var xy = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="+";
+
+//$("#to").attr('id','xx');//之后变成xx
+ 
+ 
+ 
+ 
+ var $p=Math.random();
+ 
+
+ var $z=Math.random();
+
+
+ 
+if ($p<$z)
+{
+    
+$("#"+x).val(Math.floor(Math.random()*35+1)+y2);
+
+}
+else
+{
+
+
+
+$("#"+x).after(Math.floor(Math.random()*35+1)+y2);
+
+
+  }
+      
+      
+$("#a").attr('id','f');
+
+$("#b").attr('id','e');
+
+
+$("#c").attr('id','d');
+
+
+$("#d").attr('id','c');
+
+$("#e").attr('id','b');
+
+
+$("#f").attr('id','a');
+
+
+  });
+});
+</script>
+</head>
+
+
+
+<body>
+
+
+
+<h1 style="font-size:35px;color
+
+:#FF0000" >input免输入版ms5新增，请选择上期码  ，双色球大乐透完美通用版  ____
+
+
+p4黑红版，随机加入
+     
+<br>
+
+
+<span id="sp3">____</span></h1>
+
+
+
+ <h1 style="font-size:60px;color:#FF0000"  id="">option value 免输入版可选择上期码</h1>
+ 
+ 
+ 
+<p><strong>注意:</strong> Internet Explorer 9（更早 IE 版本），Safari 不支持 datalist 标签。</p>
+ 
+
+<br>
+
+
+
+<br>
+
+
+p4黑红版，随机加入
+     
+<br>
+
+
+
+<br>
+
+
+#4025前区附加多态p4版，齐位加附加码，可复选可胆拖，p4黑红版，随机加入
+     
+<br>
+ 
+
+<br>
+
+
+
+#4025前区附加多态，齐位加附加码，可复选可胆拖，新增p4黑红版，随机加入
+     
+<br>
+
+     
+     
+<a href="ty4444tianyi2023dlt4444.php" target="_blank"  style="font-size:68px;">清空返回</a> <br> 
+
+
+
+<pre  style="font-size:60px;color:#FF0000"  id="">#新增p4黑红版2023 </pre>
+ 
+
+
+    
+ <h1>
+     
+     
+
+
+
+<br>
+
+<br>
+ 
+ 
+<button  style="font-size:60px;color:#FF0000" onclick="function()" id='button3'>前区替换还原</button>  
+
+ 
+
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+
+
+
+<button style="font-size:58px;color:#FF0000" id="btn1">前区选号</button>
+
+
+
+
+<p style="font-size:23.5px;color:#FF0000" id="">大乐透新天意新版</p>
+
+
+
+
+<button  style="font-size:58px;color:#FF0000" id="btn2">前区选号</button>
+
+
+
+
+<br>
+
+<br>
+
+
+
+<br>
+
+<br>
+
+
+
+
+<button style="font-size:68px;color:#FF0000" id="btn3">大乐透新天意前区随机</button>
+
+
+
+
+
+<br>
+
+<br>
+
+
+<br>
+
+
+<form method="post"        action="ty4444tianyi2023dlt4444.php">
+      
+
+ <div class="ui-field-contain">
+ <input   style="font-size:60px;color:#FF0000"  type="submit" data-inline="true" value="提交开始">
+
+
+
+
+<br>
+
+<br>
+
+
+
+
+
+
+<script>
+
+
+
+function myFunction1()
+{
+
+var  xy="+";
+
+
+var colors = ['a','b','c','d','e','f'];
+
+
+var x = colors[(Math.floor(Math.random() * (colors.length)))]
+
+
+
+
+
+var  y=Math.floor(Math.random()*35+1); 
+
+
+
+document.getElementById(x).innerHTML=y+xy; 
+//
+
+
+
+}
+
+
+
+</script>
+
+
+
+
+<p> <input    list="browsers" style="font-size:35px;color:#FF0000" type="text" id="a" value="">
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+
+</p>
+
+
+<p> <input   list="browsers" style="font-size:35px;color:#FF0000" type="text" id="b" value="">
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+</p>
+
+
+
+<p> <input   list="browsers" style="font-size:35px;color:#FF0000" type="text" id="c" value="">
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+</p>
+
+
+<p><input    list="browsers" style="font-size:35px;color:#FF0000" type="text" id="d" value="">
+
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+</p>
+
+
+
+<p> <input    list="browsers" style="font-size:35px;color:#FF0000" type="text" id="e" value="">
+
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+</p>
+
+
+
+
+<p><input    list="browsers" style="font-size:35px;color:#FF0000" type="text" id="f" value="">
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+</p>
+
+
+
+
+<br>
+
+<br>
+
+
+
+
+
+<p><input    list="browsers2" style="font-size:35px;color:#FF0000" type="text" id="test1" value="前区附加号码">
+
+
+<datalist id="browsers2">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+
+</datalist>
+
+
+
+
+
+<p style="font-size:35px;color:#FF0000" id=""></p>
+
+
+
+
+<p><input    list="browsers2" style="font-size:35px;color:#FF0000" type="text" id="test2" value="前区附加号码">
+
+
+<datalist id="browsers2">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+
+</datalist>
+
+
+
+
+
+<br>
+
+
+
+
+
+<br>
+
+<br>
+
+
+<br>
+
+<br>
+
+
+
+
+
+
+
+<?php
+
+
+
+?>
+
+
+</form>
+
+
+
+</body>
+</html>		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
